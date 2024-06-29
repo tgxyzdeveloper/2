632178eb36b3c42506771126439e57d48ae23070
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-06-29 20:32:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "24491383"))
 API_HASH = os.environ.get("API_HASH", "78e18eba669cc519ffd7a3c89f9ed32a")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://cogat14114:cogat14114@cluster0.amcrvsn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://mrrajib536:LoQ9PPjcRRgnQXFj@cluster0.svhwzos.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
