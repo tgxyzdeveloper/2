c73220d38160006ba5dae8f6593ca708445707ee
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-06-29 20:26:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "24491383"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "78e18eba669cc519ffd7a3c89f9ed32a")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://cogat14114:cogat14114@cluster0.amcrvsn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
